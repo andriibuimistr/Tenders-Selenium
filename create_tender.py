94d5e6e3d7ca88297714565a4dc3b1f24c08fac6
Andrii <buymistrandrey@gmail.com>
2018-03-28 03:49:02 +0300

    Add lots

--- create_tender.py
+++ create_tender.py
@@ -7,6 +7,9 @@ from initial_data.tender_additional_data import cdb_host, key_path, key_password
 import time
 import requests
 import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import json
 from datetime import datetime, timedelta
 from selenium.webdriver.common.action_chains import ActionChains
@@ -27,16 +30,17 @@ def fill_item_data(item_data, item, procurement_type):
         driver.find_element_by_name('data[items][{}][unit][value][amount]'.format(item)).send_keys(item_data['value']['amount'])
 
     # select classification
-    driver.find_element_by_xpath('//input[@name="data[items][{}][cpv_id]"]/preceding-sibling::a[contains(text(), "Визначити за довідником")]'.format(item)).click()  # open classification window
+    select_main_classification = driver.find_element_by_xpath('//input[@name="data[items][{}][cpv_id]"]/preceding-sibling::a[contains(text(), "Визначити за довідником")]'.format(item))
+    v_position = select_main_classification.location['y']
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_name('data[items][{}][description]'.format(item)))
+    select_main_classification.click()  # open classification window
     time.sleep(5)
 
-    # switch to iframe (driver.switch_to.frame for Python 3, driver.switch_to_frame for Python2)
     driver.switch_to.frame(driver.find_element_by_xpath('//div[@id="modal"]/div/div/iframe'))
     driver.find_element_by_xpath('//input[@id="search"]').send_keys(item_data['classification']['id'])
     time.sleep(2)
     driver.find_element_by_xpath('//a[contains(@id, "{}")]'.format(item_data['classification']['id'][:-4])).click()  # select classification
     driver.find_element_by_xpath('//div[@class="buttons"]/a').click()  # press select button
-    # switch to default window ('driver.switch_to.default_content' for Python 3, 'driver.switch_to_default_content' for Python 2)
     driver.switch_to.default_content()
 
     # select country
@@ -54,7 +58,7 @@ def fill_item_data(item_data, item, procurement_type):
     # delivery end date
     delivery_date = datetime.strftime(datetime.strptime(item_data['deliveryDate']['endDate'], "%Y-%m-%dT%H:%M:%S{}".format(kiev_now)), '%d/%m/%Y')
     delivery_end_date_path = driver.find_element_by_name('data[items][{}][deliveryDate][endDate]'.format(item))
-    driver.execute_script("arguments[0].scrollIntoView();", delivery_end_date_path)
+    driver.execute_script("arguments[0].scrollIntoView();", select_main_classification)
     delivery_end_date_path.click()
     driver.execute_script("arguments[0].removeAttribute('readonly','readonly')", delivery_end_date_path)
     delivery_end_date_path.clear()
@@ -63,8 +67,19 @@ def fill_item_data(item_data, item, procurement_type):
     delivery_end_date_path.send_keys(delivery_date)
 
 
+def fill_lot_data(lot_data, features_data, lot):
+    driver.find_element_by_name('data[lots][{}][title]'.format(lot)).send_keys(lot_data['title'])
+    driver.find_element_by_name('data[lots][{}][description]'.format(lot)).send_keys(lot_data['description'])
+    driver.find_element_by_name('data[lots][{}][value][amount]'.format(lot)).send_keys(lot_data['value']['amount'])
+    driver.find_element_by_name('data[lots][{}][minimalStep][amount]'.format(lot)).send_keys(lot_data['minimalStep']['amount'])
+    if 'guarantee' in lot_data:
+        driver.find_element_by_xpath('//input[@name="data[lots][{}][title]"]/ancestor::div[contains(@class, "accordionContent")]/descendant::span[contains(text(), "Електронна гарантія")][{}]'.format(lot, lot + 1)).click()
+        driver.find_element_by_name('data[lots][{}][guarantee][amount]'.format(lot)).send_keys(lot_data['guarantee']['amount'])
+
+
 def create_tender(tender_data):
     data = tender_data['data']
+    procurement_type = data['procurementMethodType']
     user_menu = driver.find_element_by_xpath('//div[contains(text(), "Мій ДЗО")]')
     hover = ActionChains(driver).move_to_element(user_menu)
     hover.perform()
@@ -94,18 +109,19 @@ def create_tender(tender_data):
         tender_type = 'features_lots'
 
     # Select tender type
-    Select(driver.find_element_by_name('tender_type')).select_by_value(tender_type)
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@class="jContent"]/div[2]/a[1]').click()  # close modal window
-    time.sleep(5)
+    if tender_type != 'simple':
+        Select(driver.find_element_by_name('tender_type')).select_by_value(tender_type)
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@class="jContent"]/div[2]/a[1]').click()  # close modal window
+        time.sleep(5)
 
     select_tax_included = driver.find_element_by_name('data[value][valueAddedTaxIncluded]')
     driver.execute_script("arguments[0].scrollIntoView();", select_tax_included)
     Select(select_tax_included).select_by_value(json.dumps(data['value']['valueAddedTaxIncluded']))
+    # select currency
+    Select(driver.find_element_by_name('data[value][currency]')).select_by_value(data['value']['currency'])
 
     if number_of_lots == 0:
-        # select currency
-        Select(driver.find_element_by_name('data[value][currency]')).select_by_value(data['value']['currency'])
         # tender amount
         driver.find_element_by_name('data[value][amount]').send_keys(data['value']['amount'])
         if 'minimalStep' in data:
@@ -116,22 +132,67 @@ def create_tender(tender_data):
 
     # tender title
     actual_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    driver.find_element_by_name('data[title]').send_keys('Limited Reporting ' + actual_time)
+    driver.find_element_by_name('data[title]').send_keys(data['title'])
 
     # tender description
-    driver.find_element_by_name('data[description]').send_keys('Limited Reporting Description')
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_name('data[description]'))
+    driver.find_element_by_name('data[description]').send_keys(data['description'])
 
     # open items section
-    items_section = driver.find_element_by_xpath('//section[@id="multiItems"]//a[@class="accordionOpen icons icon_view"]')  # items section path
+    if number_of_lots == 0:
+        section_number = 1
+    else:
+        section_number = 2
+    items_section = driver.find_element_by_xpath('(//h3[contains(text(), "Специфікація закупівлі")])[{}]/following::a[contains(@class, "accordionOpen")][1]'.format(section_number))  # items section path
     driver.execute_script("arguments[0].scrollIntoView();", items_section)  # scroll to items section
     items_section.click()
 
-    for item in range(len(data['items'])):
-        item_data = data['items'][item]
-        procurement_type = data['procurementMethodType']
-        if item > 0:
-            driver.find_element_by_xpath('//input[@name="data[items][{}][description]"]/ancestor::div[contains(@class, "listItems")]/following-sibling::table[@class="addNewItem"]/descendant::a[contains(@class, "addMultiItem")]'.format(item - 1)).click()
-        fill_item_data(item_data, item, procurement_type)
+    items = -1
+    if number_of_lots != 0:
+        for lot in range(len(data['lots'])):
+            if lot > 0:
+                driver.find_element_by_xpath('//a[contains(@class, "addMultiLot")]').click()
+            lot_data = data['lots'][lot]
+            if number_of_features != 0:
+                features_data = []
+                for feature in range(len(data['features'])):
+                    if data['features'][feature]['featureOf'] == 'lot' and data['features'][feature]['relatedItem'] == data['lots'][lot]['id']:
+                        features_data.append(data['features'][feature])
+            else:
+                features_data = []
+            fill_lot_data(lot_data, features_data, lot)
+            item_data = []
+            for item in range(len(data['items'])):
+                if data['items'][item]['relatedLot'] == data['lots'][lot]['id']:
+                    item_data.append(data['items'][item])
+            for lot_item in range(len(item_data)):
+                items += 1
+                if lot_item > 0:
+                    driver.find_element_by_xpath(
+                        '//input[@name="data[items][{}][description]"]/ancestor::div[contains(@class, "listItems")]/following-sibling::table[@class="addNewItem"]/descendant::a[contains(@class, "addMultiItem")]'.format(
+                            items - 1)).click()
+                fill_item_data(item_data[lot_item], items, procurement_type)
+    else:
+        for item in range(len(data['items'])):
+            item_data = data['items'][item]
+            if item > 0:
+                driver.find_element_by_xpath('//input[@name="data[items][{}][description]"]/ancestor::div[contains(@class, "listItems")]/following-sibling::table[@class="addNewItem"]/descendant::a[contains(@class, "addMultiItem")]'.format(item - 1)).click()
+            fill_item_data(item_data, item, procurement_type)
 
+    if procurement_type == 'belowThreshold':
+        enquiry_period_field = driver.find_element_by_name('data[enquiryPeriod][endDate]')
+        driver.execute_script("arguments[0].scrollIntoView();", enquiry_period_field)
+        enquiry_period_field.click()
+        driver.execute_script("arguments[0].removeAttribute('readonly','readonly')", enquiry_period_field)
+        enquiry_period_field.send_keys(datetime.strftime(datetime.strptime(data['enquiryPeriod']['endDate'], "%Y-%m-%dT%H:%M:%S{}".format(kiev_now)), '%d/%m/%Y'))
 
-    print(tender_data)
+    tender_period_end_date_field = driver.find_element_by_name('data[tenderPeriod][endDate]')
+    driver.execute_script("arguments[0].scrollIntoView();", tender_period_end_date_field)
+    tender_period_end_date_field.click()
+    driver.execute_script("arguments[0].removeAttribute('readonly','readonly')", tender_period_end_date_field)
+    tender_period_end_date_field.send_keys(datetime.strftime(datetime.strptime(data['tenderPeriod']['endDate'], "%Y-%m-%dT%H:%M:%S{}".format(kiev_now)), '%d/%m/%Y'))
+
+    driver.find_element_by_xpath('//button[@value="publicate"]').click()
+    if driver.find_element_by_xpath('//button[@class="js-notClean_ignore_plan"]'):
+        driver.find_element_by_xpath('//button[@class="js-notClean_ignore_plan"]').click()
+
