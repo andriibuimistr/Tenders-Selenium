ded582cbfde85eb0bd4a8e1c200555c77430fa96
Andrey <andriibuimistr@gmail.com>
2018-03-29 21:20:51 +0300

    Add screenshots for allure

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 # from config import host, driver
 from initial_data.tender_data import create_tender_data
 import pytest
+import allure
 
 
 def pytest_addoption(parser):
@@ -10,3 +11,13 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='class')
 def pmt(request):
     request.cls.pmt = create_tender_data(request.config.getoption("--pmt"))
+
+
+def pytest_exception_interact(node, call, report):
+    driver = node.instance.driver
+    # ...
+    allure.attach(
+        name='Скриншот',
+        contents=driver.get_screenshot_as_png(),
+        type=allure.constants.AttachmentType.PNG,
+    )
