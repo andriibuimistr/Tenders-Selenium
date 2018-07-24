a8f711c60245fc7d4634927a4b90c998a3bbd00e
Andrey <andriibuimistr@gmail.com>
2018-07-24 11:04:39 +0300

    Delete endpoints from some methods

--- api/cdb_requests.py
+++ api/cdb_requests.py
@@ -26,7 +26,7 @@ def save_log(code, body, resp_header, host, endpoint, method, request_name, enti
 
 
 # Send request to cdb
-def request_to_cdb(headers, host, endpoint, method, json_request, request_name, entity, is_json=True, files=None):
+def request_to_cdb(headers, host, method, json_request, request_name, entity, endpoint='', is_json=True, files=None):
     if is_json:
         json_request = json.dumps(json_request)
     attempts = 0
@@ -91,7 +91,6 @@ class TenderRequests(object):
     def publish_tender(self, json_tender):
         return request_to_cdb(headers=tender_headers_request(json_tender),
                               host=self.host,
-                              endpoint='',
                               method='POST',
                               json_request=json_tender,
                               request_name='Publish tender',
@@ -101,116 +100,115 @@ class TenderRequests(object):
         json_tender_activation = json_activate_tender(procurement_method)
         return request_to_cdb(headers=tender_headers_request(json_tender_activation),
                               host=self.host,
-                              endpoint='/{}?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=json_tender_activation,
                               request_name='Activate tender',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}?acc_token={}'.format(tender_id_long, token))
 
     def get_tender_info(self, tender_id_long):
         return request_to_cdb(headers=None,
                               host=self.host_public,
-                              endpoint='/{}'.format(tender_id_long),
                               method='GET',
                               json_request=None,
                               request_name='Get tender info',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}'.format(tender_id_long))
 
     def finish_first_stage(self, tender_id_long, token):
         return request_to_cdb(headers=tender_headers_request(json_finish_first_stage),
                               host=self.host,
-                              endpoint='/{}?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=json_finish_first_stage,
                               request_name='Finish first stage',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}?acc_token={}'.format(tender_id_long, token))
 
     def patch_second_stage(self, tender_id_long, token, json_patch_2nd_stage):
         return request_to_cdb(headers=tender_headers_request(json_patch_2nd_stage),
                               host=self.host,
-                              endpoint='/{}?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=json_patch_2nd_stage,
                               request_name='Patch 2nd stage',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}?acc_token={}'.format(tender_id_long, token))
 
     def activate_2nd_stage(self, tender_id_long, token, procurement_method):
         json_tender_activation = json_activate_tender(procurement_method)
         return request_to_cdb(headers=tender_headers_request(json_tender_activation),
                               host=self.host,
-                              endpoint='/{}?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=json_tender_activation,
                               request_name='Activate 2nd stage',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}?acc_token={}'.format(tender_id_long, token))
 
     def get_2nd_stage_info(self, tender_id_long, token):
         return request_to_cdb(headers=tender_headers_request(None),
                               host=self.host,
-                              endpoint='/{}/credentials?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=None,
                               request_name='Get 2nd stage info',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/credentials?acc_token={}'.format(tender_id_long, token))
 
     def approve_prequalification(self, tender_id_long, qualification_id, token, json_pq):
         return request_to_cdb(headers=tender_headers_request(json_pq),
                               host=self.host,
-                              endpoint='/{}/qualifications/{}?acc_token={}'.format(tender_id_long, qualification_id, token),
                               method='PATCH',
                               json_request=json_pq,
                               request_name='Approve prequalification',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/qualifications/{}?acc_token={}'.format(tender_id_long, qualification_id, token))
 
     def finish_prequalification(self, tender_id_long, token):
         return request_to_cdb(headers=tender_headers_request(json_finish_pq),
                               host=self.host,
-                              endpoint='/{}?acc_token={}'.format(tender_id_long, token),
                               method='PATCH',
                               json_request=json_finish_pq,
                               request_name='Finish prequalification',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}?acc_token={}'.format(tender_id_long, token))
 
     def activate_award_contract(self, tender_id_long, entity, entity_id, token, activation_json, count):
         return request_to_cdb(headers=tender_headers_request(activation_json),
                               host=self.host,
-                              endpoint='/{}/{}/{}?acc_token={}'.format(tender_id_long, entity, entity_id, token),
                               method='PATCH',
                               json_request=activation_json,
                               request_name='Activate {} {}'.format(entity, count),
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/{}/{}?acc_token={}'.format(tender_id_long, entity, entity_id, token))
 
     def add_supplier_limited(self, tender_id_long, token, add_supplier_json, supplier_number):
         return request_to_cdb(headers=tender_headers_request(add_supplier_json),
                               host=self.host,
-                              endpoint='/{}/awards?acc_token={}'.format(tender_id_long, token),
                               method='POST',
                               json_request=add_supplier_json,
                               request_name='Add supplier {}'.format(supplier_number),
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/awards?acc_token={}'.format(tender_id_long, token))
 
     def make_tender_bid(self, tender_id_long, bid_json, bid_number):
         return request_to_cdb(headers=tender_headers_request(bid_json),
                               host=self.host,
-                              endpoint='/{}/bids'.format(tender_id_long),
                               method='POST',
                               json_request=bid_json,
                               request_name='Publish bid {}'.format(bid_number),
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/bids'.format(tender_id_long))
 
     def activate_tender_bid(self, tender_id_long, bid_id, bid_token, activate_bid_json, bid_number):
         return request_to_cdb(headers=tender_headers_request(activate_bid_json),
                               host=self.host,
-                              endpoint='/{}/bids/{}?acc_token={}'.format(tender_id_long, bid_id, bid_token),
                               method='PATCH',
                               json_request=activate_bid_json,
                               request_name='Activate bid {}'.format(bid_number),
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/bids/{}?acc_token={}'.format(tender_id_long, bid_id, bid_token))
 
     def get_list_of_tenders(self):
         return request_to_cdb(headers=tender_headers_request(None),
                               host=self.host_public,
-                              endpoint='',
                               method='GET',
                               json_request=None,
                               request_name='Get list of tenders',
@@ -219,34 +217,33 @@ class TenderRequests(object):
     def get_bid_info(self, tender_id_long, bid_id, bid_token):
         return request_to_cdb(headers=tender_headers_request(None),
                               host=self.host,
-                              endpoint='/{}/bids/{}?acc_token={}'.format(tender_id_long, bid_id, bid_token),
                               method='GET',
                               json_request=None,
                               request_name='Get bid info',
-                              entity=self.entity)
+                              entity=self.entity,
+                              endpoint='/{}/bids/{}?acc_token={}'.format(tender_id_long, bid_id, bid_token))
 
     def get_contract_info(self, contract_id_long):
         return request_to_cdb(headers=None,
                               host=self.host_public_contracts,
-                              endpoint='/{}'.format(contract_id_long),
                               method='GET',
                               json_request=None,
                               request_name='Get contract info',
-                              entity=self.entity_contract)
+                              entity=self.entity_contract,
+                              endpoint='/{}'.format(contract_id_long))
 
     def get_contract_token(self, contract_id_long, tender_token):
         return request_to_cdb(headers=tender_headers_request(self.cdb),
                               host=self.host_contracts,
-                              endpoint='/{}/credentials?acc_token={}'.format(contract_id_long, tender_token),
                               method='PATCH',
                               json_request=None,
                               request_name='Get contract token',
-                              entity=self.entity_contract)
+                              entity=self.entity_contract,
+                              endpoint='/{}/credentials?acc_token={}'.format(contract_id_long, tender_token))
 
     def add_tender_document_to_ds(self, files):
         return request_to_cdb(headers=tender_headers_add_document_ds,
                               host=self.ds_host,
-                              endpoint='',
                               method='POST',
                               json_request=None,
                               request_name='Add document to DS (tenders)',
@@ -257,26 +254,26 @@ class TenderRequests(object):
     def add_document_from_ds_to_tender(self, tender_id_long, tender_token, json_with_document, message):
         return request_to_cdb(headers=tender_headers_patch_document_ds,
                               host=self.host,
-                              endpoint="/{}/documents?acc_token={}".format(tender_id_long, tender_token),
                               method='POST',
                               json_request=json_with_document,
                               request_name=message,
-                              entity=self.document)
+                              entity=self.document,
+                              endpoint="/{}/documents?acc_token={}".format(tender_id_long, tender_token))
 
     def add_document_from_ds_to_tender_bid(self, tender_id_long, bid_id, doc_type_url, bid_token, json_with_document, message):
         return request_to_cdb(headers=tender_headers_patch_document_ds,
                               host=self.host,
-                              endpoint="/{}/bids/{}/{}?acc_token={}".format(tender_id_long, bid_id, doc_type_url, bid_token),
                               method='POST',
                               json_request=json_with_document,
                               request_name=message,
-                              entity=self.document)
+                              entity=self.document,
+                              endpoint="/{}/bids/{}/{}?acc_token={}".format(tender_id_long, bid_id, doc_type_url, bid_token))
 
     def add_document_from_ds_to_entity(self, tender_id_long, entity_id, tender_token, json_with_document, message, entity):
         return request_to_cdb(headers=tender_headers_patch_document_ds,
                               host=self.host,
-                              endpoint="/{}/{}/{}/documents?acc_token={}".format(tender_id_long, entity, entity_id, tender_token),
                               method='POST',
                               json_request=json_with_document,
                               request_name=message,
-                              entity=self.document)
+                              entity=self.document,
+                              endpoint="/{}/{}/{}/documents?acc_token={}".format(tender_id_long, entity, entity_id, tender_token))
